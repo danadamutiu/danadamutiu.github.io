3d19e3f5af397373a6b052599309080e5b967649
danadamutiu <56245512+danadamutiu@users.noreply.github.com>
2019-11-25 12:44:37 +0200

    Add files via upload

--- html/worker/desenare.js
+++ html/worker/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "2019.11.25.2";
+document.getElementById("id_logic").innerHTML = "2019.11.25.3";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
 
@@ -34,6 +34,12 @@ function start()
 	document.getElementById("id_stop").disabled = false;
 	
 	timer_id = setInterval(desenare, 20, unghi);
+	
+	var muncitor = new Worker("prime.js");
+	muncitor.onmessage = function(e)
+	{
+		document.getElementById("id_prime").innerHTML = e.data;
+	}
 }
 
 
