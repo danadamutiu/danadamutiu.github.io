511600b67e87894ce3d83c623b16a157bc3eda1e
danadamutiu <56245512+danadamutiu@users.noreply.github.com>
2019-11-25 14:04:12 +0200

    Add files via upload

--- html/worker/desenare.js
+++ html/worker/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "2019.11.25.3";
+document.getElementById("id_logic").innerHTML = "2019.11.25.6";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
 
@@ -6,6 +6,7 @@ document.getElementById("id_stop").addEventListener("click", stop);
 var timer_id;
 var unghi = {};
 unghi.valoare = 0;
+var muncitor = null;
 
 function desenare(unghi)
 {
@@ -35,11 +36,16 @@ function start()
 	
 	timer_id = setInterval(desenare, 20, unghi);
 	
-	var muncitor = new Worker("prime.js");
-	muncitor.onmessage = function(e)
+	if (muncitor == null)
 	{
+		muncitor = new Worker("prime.js");
+		muncitor.onmessage = function(e)
 		document.getElementById("id_prime").innerHTML = e.data;
+	
+		muncitor.postMessage("start");
 	}
+	else
+		muncitor.postMessage("start");
 }
 
 
@@ -49,4 +55,13 @@ function stop()
 	document.getElementById("id_stop").disabled = true;
 	
 	clearInterval(timer_id);
+	muncitor.postMessage("stop");
 }
+
+
+
+
+
+
+
+
